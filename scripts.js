6abbe7bbed8b65c56464d88c27cb1687d2132826
Jhonatas de Freitas - Dev <jhonatas.freitas@icloud.com>
2024-12-05 00:14:02 -0300

    Mudança de cores da aplicação Web

--- scripts.js
+++ scripts.js
@@ -1,8 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Exemplo simples: Alert ao carregar a página
     alert('Bem-vindo ao Mercado Piltover!');
-
-    // Mudar a cor de fundo da seção de produtos ao clicar em um botão
+    
     const produtosSection = document.getElementById('produtos');
     produtosSection.appendChild(changeColorButton);
 });
