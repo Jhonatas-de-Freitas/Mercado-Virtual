ee1092b3afa487af24a678b9512d166b51c5c423
Jhonatas de Freitas - Dev <jhonatas.freitas@icloud.com>
2024-12-05 00:06:45 -0300

    Retirei mais um botão

--- scripts.js
+++ scripts.js
@@ -4,11 +4,5 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Mudar a cor de fundo da seção de produtos ao clicar em um botão
     const produtosSection = document.getElementById('produtos');
-    const changeColorButton = document.createElement('button');
-
-    changeColorButton.addEventListener('click', () => {
-        produtosSection.style.backgroundColor = '#FFD700';
-    });
-
     produtosSection.appendChild(changeColorButton);
 });
